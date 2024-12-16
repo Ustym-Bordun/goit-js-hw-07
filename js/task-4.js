9476628b6bfb259d014397662782d6dd91239d8b
Ustym Bordun <enderqwer12@gmail.com>
2024-12-16 02:10:39 +0200

    fix task-4

--- js/task-4.js
+++ js/task-4.js
@@ -4,19 +4,19 @@ logInForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   if (
-    !logInForm.elements.email.value.trim() ||
-    !logInForm.elements.password.value.trim()
+    logInForm.elements.email.value.trim() &&
+    logInForm.elements.password.value.trim()
   ) {
+    const formData = {
+      [logInForm.elements.email.name]: logInForm.elements.email.value.trim(),
+      [logInForm.elements.password.name]:
+        logInForm.elements.password.value.trim(),
+    };
+
+    console.log(formData);
+
+    logInForm.reset();
+  } else {
     alert("All form fields must be filled in");
   }
-
-  const formData = {
-    [logInForm.elements.email.name]: logInForm.elements.email.value.trim(),
-    [logInForm.elements.password.name]:
-      logInForm.elements.password.value.trim(),
-  };
-
-  console.log(formData);
-
-  logInForm.reset();
 });
